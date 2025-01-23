6148fc23cc30e730212b266a1ab8308cfdcd3ab5
andreasLoetzsch <andreas.lotzsch@gmail.com>
2025-01-23 14:54:41 +0100

    feat: added middleware isLoggedIn

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -21,4 +21,26 @@ const isAdmin = async (req, res, next) => {
     }
 }
 
-module.exports = {isAdmin}
+const isLoggedIn = async (req, res, next) =>{
+    const authHeader = req. headers.authorization
+    if(!authHeader){
+        return res.status(401).send('Token missing')
+    }
+    const accessToken = authHeader.split(" ")[1]
+    if (!accessToken) {
+        return res.status(401).send('Token missing');
+    }
+    try{
+        const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET_KEY)
+        req.user = decoded
+        next()
+    }catch(error){
+        if(error.name === "TokenExpiredError"){
+            return res.status(403).send('Token  expired')
+        }
+        return res.status(500).send('Server Error')
+    }
+
+} 
+
+module.exports = {isAdmin, isLoggedIn}
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,12 @@
 const {getAllUsers, getUser, deleteUser} = require('../controllers/userController')
-const {isAdmin} = require('../middleware/auth')
+const {isAdmin, isLoggedIn} = require('../middleware/auth')
 const express = require('express')
 const router = express.Router()
 
 
 
-router.get('/', getAllUsers)
-router.get('/:userId', getUser)
+router.get('/', isLoggedIn,getAllUsers)
+router.get('/:userId', isLoggedIn, getUser)
 router.delete('/:userId', isAdmin, deleteUser)
 
 module.exports = router
